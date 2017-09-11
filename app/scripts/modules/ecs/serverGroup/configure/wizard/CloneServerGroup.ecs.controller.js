166d9532c83830df162787bae298127f6067d3ca
Bruno Carrier <bruno.carrier@lookout.com>
2017-09-11 12:59:05 -0400

    added logging

--- app/scripts/modules/ecs/serverGroup/configure/wizard/CloneServerGroup.ecs.controller.js
+++ app/scripts/modules/ecs/serverGroup/configure/wizard/CloneServerGroup.ecs.controller.js
@@ -27,6 +27,7 @@ module.exports = angular.module('spinnaker.ecs.cloneServerGroup.controller', [
                                                   overrideRegistry, awsServerGroupConfigurationService,
                                                   serverGroupCommandRegistry,
                                                   serverGroupCommand, application, title) {
+    console.log('ecs controller 1');
     $scope.pages = {
       templateSelection: overrideRegistry.getTemplate('ecs.serverGroup.templateSelection', require('./templateSelection/templateSelection.html')),
       basicSettings: overrideRegistry.getTemplate('ecs.serverGroup.basicSettings', require('./location/basicSettings.html')),
@@ -48,6 +49,7 @@ module.exports = angular.module('spinnaker.ecs.cloneServerGroup.controller', [
       requiresTemplateSelection: !!serverGroupCommand.viewState.requiresTemplateSelection,
     };
 
+    console.log('ecs controller 2');
     this.templateSelectionText = {
       copied: [
         'account, region, subnet, cluster name (stack, details)',
@@ -67,6 +69,7 @@ module.exports = angular.module('spinnaker.ecs.cloneServerGroup.controller', [
 
     function onApplicationRefresh() {
       // If the user has already closed the modal, do not navigate to the new details view
+      console.log('ecs controller 3');
       if ($scope.$$destroyed) {
         return;
       }
@@ -110,6 +113,7 @@ module.exports = angular.module('spinnaker.ecs.cloneServerGroup.controller', [
     });
 
     function configureCommand() {
+      console.log('ecs controller 4');
       awsServerGroupConfigurationService.configureCommand(application, serverGroupCommand).then(function () {
         var mode = serverGroupCommand.viewState.mode;
         if (mode === 'clone' || mode === 'create') {
@@ -186,6 +190,7 @@ module.exports = angular.module('spinnaker.ecs.cloneServerGroup.controller', [
     }
 
     function initializeCommand() {
+      console.log('ecs controller 5');
       if (serverGroupCommand.viewState.imageId) {
         var foundImage = $scope.command.backingData.packageImages.filter(function(image) {
           return image.amis[serverGroupCommand.region] && image.amis[serverGroupCommand.region].includes(serverGroupCommand.viewState.imageId);
@@ -234,6 +239,7 @@ module.exports = angular.module('spinnaker.ecs.cloneServerGroup.controller', [
       $scope.state.loaded = true;
     }
 
+    console.log('ecs controller 6');
     // TODO - why did i have to remove a framgent here which is found in CloneServerGroup.aws.controller.js so this would work ???!
     // $scope.state.requiresTemplateSelection = false;
     // configureCommand();
