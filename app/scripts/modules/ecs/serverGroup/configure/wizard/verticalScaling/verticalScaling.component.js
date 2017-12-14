3b88d7034755de0c1abc27f16b5eb59705786d97
Bruno Carrier <brunopierrecarrier@gmail.com>
2017-12-14 17:44:51 -0500

    Split capacity into vertical and horizontal scaling (#35)

--- app/scripts/modules/ecs/serverGroup/configure/wizard/verticalScaling/verticalScaling.component.js
+++ app/scripts/modules/ecs/serverGroup/configure/wizard/verticalScaling/verticalScaling.component.js
@@ -5,13 +5,13 @@ const angular = require('angular');
 import { V2_MODAL_WIZARD_SERVICE } from '@spinnaker/core';
 
 module.exports = angular
-  .module('spinnaker.ecs.serverGroup.configure.wizard.autoscaling.component', [
+  .module('spinnaker.ecs.serverGroup.configure.wizard.verticalScaling.component', [
     V2_MODAL_WIZARD_SERVICE,
   ])
-  .component('ecsServerGroupAutoScaling', {
+  .component('ecsServerGroupVerticalScaling', {
     bindings: {
       command: '=',
       application: '=',
     },
-    templateUrl: require('./capacity.component.html'),
+    templateUrl: require('./verticalScaling.component.html'),
   });
